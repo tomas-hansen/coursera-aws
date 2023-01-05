36c52b132ace141c03c184457a6477a6092e2998
tomas <tomaskbot@gmail.com>
2023-01-05 02:53:34 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,6 @@
+def suma(x, y):
+    return x + y
+
+
+resultado = suma(1, 2)
+print(f"esta es la suma: {resultado}")
---
+++ test_hello.py
@@ -0,0 +1,4 @@
+from hello import suma
+
+def test_suma():
+    assert suma(1,2) == 3
